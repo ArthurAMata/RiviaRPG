6999e1de3e257e742ca154038adc1eb24deffac3
Arthur Mata <aamata2017@gmail.com>
2025-04-01 17:38:03 -0300

    criação e teste da primeira página básica
    rever a nav e a página básica

--- rivia/characters/views.py
+++ rivia/characters/views.py
@@ -1,3 +1,8 @@
+from django.views.generic import CreateView
+from characters import models, forms 
 from django.shortcuts import render
 
 # Create your views here.
+class char_creation(CreateView):
+    model = models.Character
+    template_name = 'char_creator.html'
